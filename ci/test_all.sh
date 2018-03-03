e968dabe91dedf936f6811288bb89fa71a087a0d
carenac <carena.church@gmail.com>
2018-03-02 16:19:10 -0800

    updated all

--- ci/test_all.sh
+++ ci/test_all.sh
@@ -1,4 +1,4 @@
-for notebook in "AdvancedPif" "AdvancedQueries" "ImportVASP" "IntroQueries" "MLonCitrination" "WorkingWithPIFs"; do
+for notebook in "AdvancedPif" "AdvancedQueries" "ImportInstron" "ImportVASP" "IntroQueries" "Journal Paper to Model Demo" "MLonCitrination" "t-SNE API" "WorkingWithPIFs"; do
   ~/anaconda3/bin/jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=300 --output tmp.ipynb ${notebook}.ipynb
   echo "Tested ${notebook} -^"
   rm -f tmp.ipynb
